7592ab83af730a4f2da88364378f2ed4cec39c15
Juan Olivares Fuentealba <juanandres.olivares@externos-cl.cencosud.com>
2020-10-09 21:55:46 -0300

    SMS notification when order changes of status

--- handler.js
+++ handler.js
@@ -1,18 +1,58 @@
-'use strict';
+const AWS = require('aws-sdk');
+const fetch = require("node-fetch");
+const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+AWS.config.update({ region: 'us-east-1' });
 
-module.exports.hello = async event => {
+const snsRequest = async message => {
+  const params = {
+    Message: message,
+    TopicArn: process.env.SNS_TOPIC_ARN,
+  };
+  return new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+}
+
+const saveStatus = async status => {
+  const params = {
+    TableName: 'STATUS_TABLE',
+    Item: {
+      'STATUS': { S: status }
+    }
+  };
+  return await ddb.putItem(params).promise();
+}
+
+const getLastStatus = async status => {
+  const params = {
+    TableName: 'STATUS_TABLE',
+    Key: {
+      'STATUS': { S: status }
+    },
+  };
+  return (await ddb.getItem(params).promise()).Item;
+}
+
+
+module.exports.notificator = async event => {
+  const response = await fetch(process.env.ORDER_TRACKING_URL)
+    .then(response => response.json())
+    .then(data => data);
+
+  const message = response.history.map((history) => history.status);
+  const dynamoStatus = await getLastStatus(message[0]);
+  const lastStatus = dynamoStatus === undefined ? undefined : dynamoStatus.STATUS.S;
+
+  if (lastStatus !== message[0]) {
+    await snsRequest(`La orden cambio de estado: ${message[0]}!!`);
+    await saveStatus(message[0]);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: message[0],
       },
-      null,
-      2
+      200
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
